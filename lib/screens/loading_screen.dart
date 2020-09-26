702d35a37e24f66574f08b1fff23c4fd365f99f6
Sumit Garg <gargs473@gmail.com>
2020-09-26 15:38:41 +0530

    Got the actual Weather Data from OpenWeatherMap API and used http package for Networking

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import '../services/networking.dart';
+
+const apiKey = '2646668561b9096dc79dd3db7b9baf49';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,15 +10,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async{
-    Position position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
-  }
+  double latitude;
+  double longitude;
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
+  }
+  void getLocationData() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper('api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
   }
   @override
   Widget build(BuildContext context) {
